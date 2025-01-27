33642e3da0d9a55841c60504ac8e78f905d873da
NC5002 <124633973+NC5002@users.noreply.github.com>
2025-01-27 22:45:45 +0000

    Add PlatoTipico model and register it in admin

--- proyecto_ecuador/primer_parte/models.py
+++ proyecto_ecuador/primer_parte/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class PlatoTipico(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    region = models.CharField(max_length=100)
+    precio_estimado = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre + " - "+ " $"+ str(self.precio_estimado) 
