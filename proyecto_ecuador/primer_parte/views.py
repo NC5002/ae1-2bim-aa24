411bdcd40c341d86bc1f53b3ffe3d22967a9baab
NC5002 <124633973+NC5002@users.noreply.github.com>
2025-01-27 22:59:23 +0000

    Add view and template for listing typical dishes

--- proyecto_ecuador/primer_parte/views.py
+++ proyecto_ecuador/primer_parte/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import PlatoTipico
 
-# Create your views here.
+def lista_platos(request):
+    platos = PlatoTipico.objects.all()
+    return render(request, 'primer_parte/lista_platos.html', {'platos': platos})
+
