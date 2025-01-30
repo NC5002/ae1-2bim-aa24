c34dec3e24c4c879a216138cfcf2eb8b63995c05
NC5002 <124633973+NC5002@users.noreply.github.com>
2025-01-30 04:00:12 +0000

    Add functionality to remove dishes from the shopping cart and style the delete button

--- proyecto_ecuador/primer_parte/urls.py
+++ proyecto_ecuador/primer_parte/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.lista_platos, name='lista_platos'),
     path('agregar_al_carrito/<int:plato_id>/', views.agregar_al_carrito, name='agregar_al_carrito'),
     path('carrito/', views.ver_carrito, name='ver_carrito'),
+    path('eliminar_del_carrito/<int:plato_id>/', views.eliminar_del_carrito, name='eliminar_del_carrito'),
 ]
--- proyecto_ecuador/primer_parte/views.py
+++ proyecto_ecuador/primer_parte/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import PlatoTipico
 
 def lista_platos(request):
@@ -17,3 +17,15 @@ def ver_carrito(request):
     carrito = PlatoTipico.objects.filter(id__in=carrito_ids)
     total = sum(plato.precio_estimado for plato in carrito)
     return render(request, 'primer_parte/carrito.html', {'carrito': carrito, 'total': total})
+
+#funcion para eliminar un plato del carrito
+def eliminar_del_carrito(request, plato_id):
+    plato = get_object_or_404(PlatoTipico, id=plato_id)
+    carrito = request.session.get('carrito', [])
+    
+    # Eliminar el plato del carrito
+    if plato.id in carrito:
+        carrito.remove(plato.id)
+        request.session['carrito'] = carrito
+    
+    return redirect('ver_carrito')
